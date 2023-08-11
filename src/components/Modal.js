a50f26fd96197664a39cb4bf8ca7fc8b2ac1438c
Kaan Fırat <kaanfrat@icloud.com>
2023-08-11 21:05:11 +0300

    chore: refactor nextConfig

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Modal = ({ isVisible, onClose, children }) => {
   if (!isVisible) return null;
+  const scrollable = useRef(null);
   const customStyles = {
     overlay: {
        backgroundColor: 'rgba(0, 0, 0, 0.6)'
@@ -15,32 +16,37 @@ const Modal = ({ isVisible, onClose, children }) => {
        transform: 'translate(-50%, -50%)'
     },
     modal: {
-      width: "100%",
-      backgroundColor: "#fff",
-      boxShadow: [0, 0, "0.625rem", "rgba(0, 0, 0, 0.2)"],
-      position: "relative",
-      padding: "1rem",
-  
-      "@media (min-width: 576px)": {
-        width: "32rem"
-      },
-  
-      "& p:last-of-type": {
-        marginBottom: 0
-      }
+    width: "100%",
+    backgroundColor: "#fff",
+    boxShadow: [0, 0, "0.625rem", "rgba(0, 0, 0, 0.2)"],
+    position: "relative",
+    padding: "1rem",
+
+    "@media (min-width: 576px)": {
+      width: "32rem"
+    },
+
+    "@media (max-width: 480px)" :{
+      height: "100px"
+    } ,
+
+    "& p:last-of-type": {
+      marginBottom: 0
     }
+  },
  }
   return (
     <div
       className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center"
+    
       onClick={onClose}
     >
       <div
-        className="modal flex"
+        className="w-[600px]  flex"
         onClick={(e) => e.stopPropagation()}
       >
         <div className="rounded-lg bg-neutral-50 text-neutral-900 shadow">
-          <div className="flex justify-end ">
+          <div className=" justify-end ">
           </div>
           {children}
         </div>
